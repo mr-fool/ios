c5fbae54a4f668d1fe145a1c81678098ee0196c0
mr-fool <mr-fool@users.noreply.github.com>
2018-10-04 15:58:15 -0600

    Add files via upload

---
+++ presentation1-p6/presentation1-p6/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  presentation1-p6
+//
+//  Created by Riike on 2018-10-02.
+//  Copyright © 2018 Riike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    
+    }
+
+
+}
+
